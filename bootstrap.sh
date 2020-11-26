7eac6b2f7a2418c6a1255b256341336b7b988fda
Mustafa Kemal GILOR <mgilor@nettsi.com>
2020-11-26 11:01:08 +0300

    Final tidy-up

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,23 +26,73 @@ SCRIPT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 # Enable abort on error
 set -eu
 
+readonly BASH_GIT_PROMPT_REPO_URL=https://github.com/magicmonty/bash-git-prompt.git
+
+# ______________________________________________________
+# APT
+# ______________________________________________________
+readonly apt_command='apt-get'
+readonly apt_args='-y install --no-install-recommends'
+
+# Packages to be installed via apt
+# TODO(mgilor): In order to save space, it might be a good idea to separate these tools 
+# to their own containers, then layer them as needed (make them opt-in).
+readonly apt_package_list=(
+    # Prerequisites
+    apt-utils dialog sudo
+    # Editors
+    nano vim
+    # Verify ssh, git, git-flow, git-lfs process tools, lsb-release (useful for CLI installs) installed
+    ssh git git-flow git-lfs iproute2 procps lsb-release
+    # Install GNU GCC Toolchain, version 10
+    gcc-10 g++-10 gdb libstdc++-10-dev libc6-dev
+    # Install LLVM Toolchain, version 10
+    llvm-10 lldb-10 clang-10 clangd libc++-10-dev
+    # Install build generator & dependency resolution and build accelarator tools
+    make ninja-build autoconf automake libtool m4 cmake ccache
+    # Install python & pip
+    python3 python3-pip
+    # Install static analyzers, formatting, tidying,
+    clang-format-10 clang-tidy-10 iwyu cppcheck
+    # Unit test, mock and benchmark
+    libgtest-dev libgmock-dev libbenchmark-dev
+    # Debugging/tracing
+    valgrind
+    # Install code coverage
+    lcov gcovr
+    # Documentation & graphing
+    doxygen doxygen-doxyparse graphviz
+    # Miscallenaous utilities
+    bash-completion
+)
+
+# ______________________________________________________
+# PIP
+# ______________________________________________________
+readonly pip_command='pip3'
+readonly pip_args='install'
+# Packages to be installed via pip
+readonly pip_package_list=(
+    conan
+    requests
+)
+
+# ______________________________________________________
+# CONAN
+# ______________________________________________________
+readonly conan_command='conan'
+# Packages to be installed via Conan
+readonly conan_package_list=(
+    gtest/1.10.0
+    benchmark/1.5.2
+)
+
 echo "Current user is $(whoami)"
 
-# run bash function as super user
-# no params
-function sufu {
-    local firstArg=$1
-    if [ $(type -t $firstArg) = function ]
-    then
-        shift && command sudo bash -c "$(declare -f $firstArg);$firstArg $*"
-    elif [ $(type -t $firstArg) = alias ]
-    then
-        alias sudo='\sudo '
-        eval "sudo $@"
-    else
-        command sudo "$@"
-    fi
-}
+
+# ______________________________________________________
+# Utility functions
+# ______________________________________________________
 
 # username useruid usergid
 function add_user {
@@ -62,12 +112,14 @@ function switch_to_user {
 }
 # no params
 function apt_cleanup {
-    ( $apt_command autoremove -y && $apt_command clean -y && rm -rf /var/lib/apt/lists/* ) || return $?
+    ( sudo ${apt_command} autoremove -y && sudo  ${apt_command} clean -y && sudo rm -rf /var/lib/apt/lists/* ) || return $?
     return 0
 }
 # no params
 function pip_cleanup {
-    return pip cache purge || true
+    ( pip cache purge ) || return $?
+    ( sudo pip cache purge ) || return $?
+    return 0
 }
 
 #;
@@ -76,10 +128,9 @@ function pip_cleanup {
 # @return void
 #"
 function cleanup {
-    sufu apt_cleanup
-    sufu pip_cleanup 
-    pip_cleanup 
-    # Clean all temporary files
+    apt_cleanup
+    pip_cleanup $1
+    # # Clean all temporary files
     sudo rm -rf /tmp/*
 }
 
@@ -90,7 +141,6 @@ function cleanup {
 # @return error code
 #"
 function install_bash_git_prompt {
-    readonly BASH_GIT_PROMPT_REPO_URL=https://github.com/magicmonty/bash-git-prompt.git
     # Install "bash git prompt"
     git clone --depth=1 -- ${BASH_GIT_PROMPT_REPO_URL} /home/$1/.bash-git-prompt && \
  cat <<EOF >> /home/${1}/.bashrc 
@@ -105,55 +155,14 @@ EOF
 }
 
 function install_apt_packages {
-    readonly apt_command='apt-get'
-    readonly apt_args='-y install --no-install-recommends'
-    # Packages to be installed via apt
-    # TODO(mgilor): In order to save space, it might be a good idea to separate these tools 
-    # to their own containers, then layer them as needed (make them opt-in).
-    readonly apt_package_list=(
-        # Prerequisites
-        apt-utils dialog sudo
-        # Editors
-        nano vim
-        # Verify ssh, git, git-flow, git-lfs process tools, lsb-release (useful for CLI installs) installed
-        ssh git git-flow git-lfs iproute2 procps lsb-release
-        # Install GNU GCC Toolchain, version 10
-        gcc-10 g++-10 gdb libstdc++-10-dev libc6-dev
-        # Install LLVM Toolchain, version 10
-        llvm-10 lldb-10 clang-10 clangd libc++-10-dev
-        # Install build generator & dependency resolution and build accelarator tools
-        make ninja-build autoconf automake libtool m4 cmake ccache
-        # Install python & pip
-        python3 python3-pip
-        # Install static analyzers, formatting, tidying,
-        clang-format-10 clang-tidy-10 iwyu cppcheck
-        # Unit test, mock and benchmark
-        libgtest-dev libgmock-dev libbenchmark-dev
-        # Debugging/tracing
-        valgrind
-        # Install code coverage
-        lcov gcovr
-        # Documentation & graphing
-        doxygen doxygen-doxyparse graphviz
-        # Miscallenaous utilities
-        bash-completion
-    )
-
     echo "Installing apt packages..."
     ( ${apt_command} update && ${apt_command} ${apt_args} ${apt_package_list[@]} ) || return $?
     return 0
 }
 
 function install_pip_packages {
-    readonly pip_command='pip3'
-    readonly pip_args='install'
-    # Packages to be installed via pip
-    readonly pip_package_list=(
-        conan
-        requests
-    )
     echo "Installing pip packages..."
-    ( $pip_command $pip_args ${pip_package_list[@]} ) || return $?
+    ( ${pip_command} ${pip_args} ${pip_package_list[@]} ) || return $?
     return 0
 }
 
@@ -164,12 +173,12 @@ function install_pip_packages {
 # @return error code
 #"
 function install_conan_packages {
-    readonly conan_command='conan'
     echo "Installing conan packages..."
+
     # Create new default conan profile
     ( sudo su ${1} -c "${conan_command} profile new default --detect" \
     &&
-    sudo su ${1}-c "${conan_command} profile update settings.os_target=Linux default" \
+    sudo su ${1} -c "${conan_command} profile update settings.os_target=Linux default" \
     &&
     sudo su ${1} -c "${conan_command} profile update settings.arch_target=x86_64 default" \
     &&
@@ -181,16 +190,16 @@ function install_conan_packages {
     &&
     sudo su ${1} -c "${conan_command} profile update settings.cppstd=20 default" \
     &&
-    sudo su ${1} -c "${conan_command} profile update settings.build_type=RelWithDebInfo default" \
-    &&
-    sudo su ${1} -c "${conan_command} install gtest/1.10.0@_/_ --build missing"  \
-    &&
-    sudo su ${1} -c "${conan_command} install gtest/1.10.0@_/_ -s build_type=Debug --build missing" \
-    &&
-    sudo su ${1} -c "${conan_command} install benchmark/1.5.2@_/_ --build missing" \
-    &&
-    sudo su ${1} -c "${conan_command} install benchmark/1.5.2@_/_ -s build_type=Debug --build missing" ) \
+    sudo su ${1} -c "${conan_command} profile update settings.build_type=RelWithDebInfo default" ) \
     || return $?
+
+    # Install conan packages
+    for pkg in "${conan_package_list[@]}"
+    do
+        (sudo su ${1} -c "${conan_command} install ${pkg}@_/_ --build missing" \
+        &&
+        sudo su ${1} -c "${conan_command} install ${pkg}@_/_ -s build_type=Debug --build missing") || return $?
+    done
     return 0
 }
 
@@ -208,22 +217,29 @@ function adjust_symlinks {
     sudo ln -sf /usr/bin/python3 /usr/bin/python
 }
 
-( install_apt_packages \
-&&
-add_user ${USERNAME} ${USER_UID} ${USER_GID} \
-&&
-make_user_sudoer $USERNAME \
-&&
-switch_to_user $USERNAME \
-&& 
-install_pip_packages \
-&&
-install_bash_git_prompt $USERNAME \
-&& 
-install_conan_packages $USERNAME \
-&& 
-adjust_symlinks \
-&&
-cleanup) || exit 1
+
+# ______________________________________________________
+# Main routine
+# ______________________________________________________
+
+( 
+    install_apt_packages \
+    &&
+    add_user ${USERNAME} ${USER_UID} ${USER_GID} \
+    &&
+    make_user_sudoer $USERNAME \
+    &&
+    switch_to_user $USERNAME \
+    && 
+    install_pip_packages $USERNAME \
+    &&
+    install_bash_git_prompt $USERNAME \
+    && 
+    install_conan_packages $USERNAME \
+    && 
+    adjust_symlinks \
+    &&
+    cleanup $USERNAME 
+) || exit 1
 
 echo "Current user is $(whoami)"
